ccd5c5ad3a9a3ceb26fe9210fb2e5ea5552dcaa1
Suvarna Jadhav <jadhav.suvarna28@gmail.com>
2023-02-07 09:51:23 -0500

    Implemented view all employees

--- db/query.sql
+++ db/query.sql
@@ -11,6 +11,22 @@ FROM employee a
 LEFT JOIN employee b ON a.id = b.manager_id
 WHERE b.manager_id IS NOT NULL
 
+--View all roles
 SELECT employeerole.id, employeerole.title, department.dept_name, employeerole.salary
 FROM employeerole
 JOIN department ON employeerole.department_id = department.id;
+
+--Total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+SELECT department.dept_name, sum(employeerole.salary)
+FROM employee
+JOIN employeerole ON employee.role_id = employeerole.id
+JOIN department ON employeerole.department_id = department.id
+GROUP BY department.dept_name;
+
+--View all employees
+SELECT a.id, a.first_name, a.last_name, 
+employeerole.title, department.dept_name, employeerole.salary, CONCAT(b.first_name, ' ', b.last_name) as manager
+FROM employee a
+LEFT JOIN employeerole ON a.role_id = employeerole.id
+LEFT JOIN department ON employeerole.department_id = department.id
+LEFT JOIN employee b ON a.manager_id = b.id;
--- index.js
+++ index.js
@@ -26,18 +26,28 @@ const commonQuestions = [{
     name: 'deptname',
     message: 'What is the name of the department?',
 },
+{
+    type: 'input',
+    name: 'rolename',
+    message: 'What is the name of the role?',
+},
+{
+    type: 'input',
+    name: 'rolesalary',
+    message: 'What is the salary of the role?',
+},
 ];
 
-function init() { 
+function init() {
     let employee = "Employee";
-    let employee_ascii =  asciitextgenerator(employee, "2");
+    let employee_ascii = asciitextgenerator(employee, "2");
     let manager = "Manager";
     let manager_ascii = asciitextgenerator(manager, "2");
-    
+
     console.log(
         box(employee_ascii +
             manager_ascii));
-    
+
     inquirer.prompt([
         commonQuestions[0]
     ])
@@ -54,26 +64,31 @@ function selectChoice(data) {
         case 'View all roles':
             viewAllRoles();
             break;
-        // case 'View all employees':
-        //     cactussucculentArray(data[i].common, latinName);
-        //     break;
-        // case 'Add an employee':
-        //     flowerArray(data[i].common, latinName);
-        //     break;
+        case 'View all employees':
+            viewAllEmployees();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
         case 'Add a department':
             addDepartment();
             break;
-        // case 'Add a role':
-        //     palmArray(data[i].common, latinName);
-        //     break;
+        case 'Add a role':
+            addRole();
+            break;
         // case 'Update an employee role':
         //     palmArray(data[i].common, latinName);
         //     break;
     }
 }
 
-function viewAllDepartments() {
-    const sql = `SELECT * FROM department`;
+function viewAllEmployees() { 
+    const sql = `SELECT a.id, a.first_name, a.last_name, 
+                employeerole.title, department.dept_name, employeerole.salary, CONCAT(b.first_name, ' ', b.last_name) as manager
+                FROM employee a
+                LEFT JOIN employeerole ON a.role_id = employeerole.id
+                LEFT JOIN department ON employeerole.department_id = department.id
+                LEFT JOIN employee b ON a.manager_id = b.id;`;
     db.query(sql, (err, rows) => {
         if (err) throw err;
         const table = cTable.getTable(rows);
@@ -87,10 +102,8 @@ function viewAllDepartments() {
     });
 }
 
-function viewAllRoles() {
-    const sql = `SELECT employeerole.id, employeerole.title, department.dept_name, employeerole.salary
-                FROM employeerole
-                JOIN department ON employeerole.department_id = department.id`;
+function viewAllDepartments() {
+    const sql = `SELECT * FROM department`;
     db.query(sql, (err, rows) => {
         if (err) throw err;
         const table = cTable.getTable(rows);
@@ -114,6 +127,92 @@ function addDepartment() {
                 if (err) throw err;
             });
             console.log(`Added ${data.deptname} to the database`);
+            inquirer.prompt([
+                commonQuestions[0]
+            ])
+                .then((data) => {
+                    selectChoice(data);
+                });
+        });
+}
+
+function viewAllRoles() {
+    const sql = `SELECT employeerole.id, employeerole.title, department.dept_name, employeerole.salary
+                FROM employeerole
+                JOIN department ON employeerole.department_id = department.id`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        const table = cTable.getTable(rows);
+        console.log(table);
+        inquirer.prompt([
+            commonQuestions[0]
+        ])
+            .then((data) => {
+                selectChoice(data);
+            });
+    });
+}
+
+function addRole() {
+    let deptArray = [];
+    const sql = `SELECT dept_name FROM department`;
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        rows.forEach(x => deptArray.push(x['dept_name']));
+    });
+    inquirer.prompt([
+        commonQuestions[2],
+        commonQuestions[3],
+        {
+            type: 'list',
+            name: 'roledept',
+            message: 'Which department does the role belong too?',
+            choices: deptArray
+        },
+    ])
+        .then((data) => {
+            let sql = `INSERT INTO employeerole (title, salary) VALUES ("${data.rolename}", "${data.rolesalary}")`;
+            db.query(sql, (err, rows) => {
+                if (err) throw err;
+            });
+            console.log(`Added ${data.rolename} to the database`);
+            inquirer.prompt([
+                commonQuestions[0]
+            ])
+                .then((data) => {
+                    selectChoice(data);
+                });
+        });
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeefirstname',
+            message: 'What is the employee\'s first name?'
+        },
+        {
+            type: 'input',
+            name: 'employeelastname',
+            message: 'What is the employee\'s last name?'
+        },
+        {
+            type: 'input',
+            name: 'employeerole',
+            message: 'What is the employee\'s role?'
+        },
+        {
+            type: 'input',
+            name: 'employeemanager',
+            message: 'Who is the employee\'s manager?'
+        },
+    ])
+        .then((data) => {
+            let sql = `INSERT INTO employee (first_name, last_name) VALUES ("${data.employeefirstname}", "${data.employeefirstname}")`;
+            db.query(sql, (err, rows) => {
+                if (err) throw err;
+            });
             inquirer.prompt([
                 commonQuestions[0]
             ])
